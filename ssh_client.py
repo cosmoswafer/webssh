bb716d6f2307cd54fb1c010b883be409677dfa2d
Jimmy.|\| <jimmy09140456@gmail.com>
2026-05-06 17:41:23 +0800

    Add SSH client helper for screen session startup

--- ssh_client.py
+++ ssh_client.py
@@ -118,6 +118,9 @@ class SSHClient:
             None, self.channel.send, data
         )
 
+    async def start_screen_session(self):
+        await self.send_input("screen -r || screen\n")
+
     async def handle_resize(self, cols, rows):
         await asyncio.get_event_loop().run_in_executor(
             None, self.channel.resize_pty, cols, rows
